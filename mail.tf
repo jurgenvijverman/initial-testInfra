7ea8b315b535de12519a9b4ba0a0991b159af2d0
jurgenvijverman <63164608+jurgenvijverman@users.noreply.github.com>
2020-04-06 12:59:03 +0200

    Update mail.tf

--- mail.tf
+++ mail.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "eu-central-1"
 }
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-0b418580298265d5c"
   instance_type = "t2.micro"
 }
