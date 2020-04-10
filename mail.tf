593cf38531889b50d7282efbc2165bcab0f31ef9
Jurgen Vijverman <jurgen@Jurgens-MBP.lan>
2020-04-10 18:29:35 +0200

    first change commited through vscode

--- mail.tf
+++ mail.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-029ab77b83e832e7d"
-  instance_type = "t2.micro"
-  key_name = "awsInstances"
-}
+# resource "aws_instance" "example" {
+#  ami           = "ami-029ab77b83e832e7d"
+#  instance_type = "t2.micro"
+#  key_name = "awsInstances"
+#}
 
